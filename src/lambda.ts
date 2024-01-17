90fa4af0b216e68d08ae6a00f26b31950ce011bc
Thomas Rogers <podginator@gmail.com>
2024-01-17 21:37:34 +0100

    Messed up the logic

--- src/lambda.ts
+++ src/lambda.ts
@@ -9,7 +9,7 @@ export const handler = async (
 ): Promise<void> => {
   logger.log(`Retrieved Page: ${event.page.title}, Trying to get Labels...`)
   logger.log(JSON.stringify(event))
-  if (event.page.state != 'SUCCEEDED' || event.action != 'updated') {
+  if (event.page.state != 'SUCCEEDED' && event.action != 'updated') {
     logger.error('Not able to parse an incomplete article, exiting early...')
     return
   }
